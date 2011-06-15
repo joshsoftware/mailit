3ddefc1ab5346dc844cc7759914fe68f57b235a9
Ninad Pol <ninad@server.joshsoftware.com>
2011-06-15 16:41:53 +0530

    Added action for sending newsletters through UI

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -15,6 +15,46 @@ class SubscribersController < ApplicationController
 
 
   def send_mailers
+    @news=Newsletter.new(:mailer_subject => params[:subject],:mailer_template => params[:upload_template], :type_of_mailer => params[:send_mail])
+      if @news.valid?
+         #@news.save
+         #flash[:notice] = "Mailer sent sucessfully."
+          
+          #Test_mailer
+          if params[:send_mail] == "test" and !params[:test_address].blank?
+             #Separate the comma separated email id's
+             params[:test_email_address].split(",").each do |email|
+             unique_identifier = Digest::MD5.hexdigest(email)
+             Notifier.deliver_massmailer(params[:subject], params[:upload_template], email,unique_identifier)
+             end
+          #Mailer to subscribed users in the database
+          elsif params[:send_mail] == "database"
+             count = 0
+             Subscriber.find(:all, :conditions => ["is_subscribed = true"]).each do |subscriber|
+               begin
+               Notifier.deliver_massmailer(params[:subject], params[:upload_template], subscriber.email,subscriber.unique_identifier)
+               count += 1
+               rescue
+                puts "#{subscriber.login} - Error in sending to #{subscriber.email}"
+               end
+             end
+             puts "Count of subscribed users:#{Subscriber.find(:all, :conditions => ["is_subscribed = true"]).size}"
+             puts "Mail sent to #{count} users"
+          #Mailer to external db 
+          elsif params[:send_mail] == "csv" and !params[:csv_upload].blank?
+
+      
+          else
+            flash[:notice] = "Please enter all mandatory fields(*)"
+            #redirect_to :controller => 'subscribers', :action => 'send_mailers'
+          end
+      else
+         flash[:error] = @news.errors.full_messages.join(', ')
+      end
+
+    render :action => :newsletters
+
+=begin
     #check if the fields "subject" & "upload_template" are not blank
     if !params[:subject].blank? and !params[:upload_template].blank?
       #Test_mailer
@@ -51,6 +91,7 @@ class SubscribersController < ApplicationController
       flash[:notice] = "Please enter all mandatory fields(*)"
       render :action => :newsletters
     end 
+=end
   end
 
 end
--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -10,6 +10,7 @@ class Notifier < ActionMailer::Base
         @code=code_to_unsubscribe
    end
 
+=begin
    # Method to be used for Final mailer
    # Here the parameter "receiver" will be subscriber-user object
    def massmailer(receiver)
@@ -19,6 +20,16 @@ class Notifier < ActionMailer::Base
        @subject = "Josh Software | Newsletter | #{@dt}"
        @code=receiver.unique_identifier
    end
+=end
+
+  def massmailer(subject,mailer_template,email,uid)
+       setup_email(email )
+       #current date value on which mailer will be sent
+       @dt=Date.today.strftime("%B %G")
+       @subject = subject
+       @code=uid
+   end
+
  
    # Same method below can be used for test & final mailers
    protected
