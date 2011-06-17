8a7611013787a4d52cb5007c1db31efbb09c88d5
Ninad <ninad@ninad.(none)>
2011-06-17 19:32:55 +0530

    updated send_mailer action and added basic authentication

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,11 +1,16 @@
-class SubscribersController < ApplicationController
+require 'csv'
 
+class SubscribersController < ApplicationController
+  
+  USER, PASSWORD = 'newsletter', 'josh123'
+  before_filter :authentication_check, :only => [:newsletters,:send_mailers]
+
+            
   #Method to unsubscribe the user from newsletter on the basis of incoming uid
   def unsubscribe
     user = Subscriber.find_by_unique_identifier(params[:id])
     user.update_attribute(:is_subscribed, false)
     user.save!
-
     render
   end
 
@@ -19,13 +24,19 @@ class SubscribersController < ApplicationController
       if @news.valid?
          #@news.save
          #flash[:notice] = "Mailer sent sucessfully."
-          
+          p "valid..."
           #Test_mailer
-          if params[:send_mail] == "test" and !params[:test_address].blank?
+          if params[:send_mail] == "test" and !params[:test_email_address].blank?
+            p "inside test..."
              #Separate the comma separated email id's
              params[:test_email_address].split(",").each do |email|
              unique_identifier = Digest::MD5.hexdigest(email)
-             Notifier.deliver_massmailer(params[:subject], params[:upload_template], email,unique_identifier)
+               begin
+                 Notifier.deliver_massmailer(params[:subject], params[:upload_template], email,unique_identifier)
+                 flash[:notice] = "Email sent successfully"
+               rescue Exception => e
+                 puts "Error:=>#{e.message}"
+               end  
              end
           #Mailer to subscribed users in the database
           elsif params[:send_mail] == "database"
@@ -38,17 +49,34 @@ class SubscribersController < ApplicationController
                 puts "#{subscriber.login} - Error in sending to #{subscriber.email}"
                end
              end
-             puts "Count of subscribed users:#{Subscriber.find(:all, :conditions => ["is_subscribed = true"]).size}"
+             puts "Count of subscribed users:#{Subscriber.find(:all, :conditions => ["iis_subscribed = true"]).size}"
              puts "Mail sent to #{count} users"
           #Mailer to external db 
-          elsif params[:send_mail] == "csv" and !params[:csv_upload].blank?
+          elsif params[:send_mail] == "externaldb" and !params[:csv_upload].blank?
+             p "inside csv..."
+             begin
+               @uploaded_csv=CSV::Reader.parse(params[:csv_upload])
+               @uploaded_csv.each do |row|
+                  unique_identifier = Digest::MD5.hexdigest(row[2])
+                  begin
+                    Notifier.deliver_massmailer(params[:subject], params[:upload_template], row[2],unique_identifier)
+                    flash[:notice] = "Email sent successfully"
+                  rescue Exception => e
+                    puts "Error:=>#{e.message}"
+                  end  
+               end
+             rescue Exception => e
+                 puts "Error:=>#{e.message}"
+                 flash[:error] = "Invalid csv file.Please correct it & try again."
+             end  
 
-      
           else
-            flash[:notice] = "Please enter all mandatory fields(*)"
+            p "inside else1..."
+            flash[:error] = "Please enter all mandatory fields(*)"
             #redirect_to :controller => 'subscribers', :action => 'send_mailers'
           end
       else
+         p "inside else2..."
          flash[:error] = @news.errors.full_messages.join(', ')
       end
 
@@ -92,6 +120,13 @@ class SubscribersController < ApplicationController
       render :action => :newsletters
     end 
 =end
+  end
+
+  private
+  def authentication_check
+      authenticate_or_request_with_http_basic do |user, password|
+      user == USER && password == PASSWORD
+      end
   end
 
 end
