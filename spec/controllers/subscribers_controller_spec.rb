0815992fe71210680e1de2281e77c7148a995f2b
Ninad <ninad@ninad.(none)>
2011-06-20 18:23:29 +0530

    Added check to by pass basic authentication and added cases for sending mails

--- spec/controllers/subscribers_controller_spec.rb
+++ spec/controllers/subscribers_controller_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe SubscribersController do
   let(:sub_user) { Factory(:subscriber) }
 
+  #This call is necessary to by_pass the basic authentication added while running your specs for send_mailers action
+  before(:each) do
+    by_pass_basic_authentication
+  end
+
+
   it "should unsubscribe the user" do
     #Started GET "/unsubscribe/01ac5133869dbe739fe5745aacc15900"
     get :unsubscribe, :id => sub_user.unique_identifier
@@ -37,21 +43,29 @@ describe SubscribersController do
        post :send_mailers,:subject =>'TestSubject123',:upload_template =>fixture_file_upload("test.html", mime_type = nil, binary = false),:send_mail =>'externaldb'
        flash[:error].should =="Please enter all mandatory fields(*)"
    end
-
  end
 
  context "should send" do
   
    it "test mail to single user" do
-       post :send_mailers,:subject =>'TestSubject123',:upload_template =>fixture_file_upload("test.html", mime_type = nil, binary = false),:send_mail =>'externaldb',:email_address=>'joshsoftwaretest1@gmail.com'
+       post :send_mailers,:subject =>'TestSubject123',:upload_template =>fixture_file_upload("test.html", mime_type = nil, binary = false),:send_mail =>'test',:test_email_address=>'joshsoftwaretest1@gmail.com'
        flash[:notice].should=="Email sent successfully"
    end
 
    it "test mail to mulitple users" do
-       post :send_mailers,:subject =>'TestSubject123',:upload_template =>fixture_file_upload("test.html", mime_type = nil, binary = false),:send_mail =>'externaldb',:email_address=>'joshsoftwaretest1@gmail.com,joshsoftwaretest2@gmail.com'
+       post :send_mailers,:subject =>'TestSubject123',:upload_template =>fixture_file_upload("test.html", mime_type = nil, binary = false),:send_mail =>'test',:test_email_address=>'joshsoftwaretest1@gmail.com,joshsoftwaretest2@gmail.com'
       flash[:notice].should=="Email sent successfully"
    end    
 
+   it "mail to database" do
+       post :send_mailers,:subject =>'TestSubject123',:upload_template =>fixture_file_upload("test.html", mime_type = nil, binary = false),:send_mail =>'database'
+       flash[:notice].should=="Email sent successfully"
+   end
+
+   it "mail to external database" do
+       post :send_mailers,:subject =>'TestSubject123',:upload_template =>fixture_file_upload("test.html", mime_type = nil, binary = false),:send_mail =>'externaldb',:csv_upload =>fixture_file_upload("spec/test.csv", mime_type = nil, binary = false)
+       flash[:notice].should=="Email sent successfully"
+   end
  end
 
 end
