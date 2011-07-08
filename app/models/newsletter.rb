0da042dc36c043354ae18d62e2de1d238d0703cc
Ninad <ninad@ninad.(none)>
2011-07-08 19:51:29 +0530

    removed validity for removed field and added validation for paperclip attachment

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,3 +1,7 @@
 class Newsletter < ActiveRecord::Base
-  validates_presence_of :mailer_subject,:mailer_template
+  validates_presence_of :mailer_subject
+  validates_attachment_presence :template
+
+  # Paperclip
+  has_attached_file :template
 end
