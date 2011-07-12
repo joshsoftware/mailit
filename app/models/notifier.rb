c87272e0e675af6b6e528d9a32d5ac185956f157
Ninad <ninad@ninad.(none)>
2011-07-12 17:29:51 +0530

    updating notifier method to pass path of the uploaded template

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -10,7 +10,7 @@ class Notifier < ActionMailer::Base
         @code=code_to_unsubscribe
    end
 
-
+=begin
    # Method to be used for Final mailer
    # Here the parameter "receiver" will be subscriber-user object
    def massmailer(receiver)
@@ -20,24 +20,26 @@ class Notifier < ActionMailer::Base
        @subject = "Josh Software | Newsletter | #{@dt}"
        @code=receiver.unique_identifier
    end
+=end   
 
-=begin
-  def massmailer(subject,mailer_template,email,uid)
+
+  def massmailer(subject,mailer_template_to_render,email,uid)
        setup_email(email )
        #current date value on which mailer will be sent
        @dt=Date.today.strftime("%B %G")
        @subject = subject
        @code=uid
-   end
-=end 
-   # Same method below can be used for test & final mailers
-   protected
-   def setup_email(mail)
-     @from="Josh Software<marketing@joshsoftware.com>"
-     @recipients= mail
-     @sent_on = Time.now
-     @content_type = "text/html"
-   end 
+       @tmplte=mailer_template_to_render
+  end 
+
+  # Same method below can be used for test & final mailers
+  protected
+  def setup_email(mail)
+      @from="Josh Software<marketing@joshsoftware.com>"
+      @recipients= mail
+      @sent_on = Time.now
+      @content_type = "text/html"
+  end 
 
 end
 
