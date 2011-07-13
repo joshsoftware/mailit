bb739c2decdf5ff48fe36ad514cf3dd4f4723e67
Ninad <ninad@ninad.(none)>
2011-07-13 19:19:04 +0530

    added attribute accessor for template file name required for spec

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,7 +1,8 @@
 class Newsletter < ActiveRecord::Base
   validates_presence_of :mailer_subject
   validates_attachment_presence :template
-
+  
   # Paperclip
+  attr_accessor :template_file_name 
   has_attached_file :template
 end
