1b8819780c57f30f42d5c46aaf2fe64e76eec602
Ninad Pol <ninad@joshsoftware.com>
2011-11-16 19:38:12 +0530

    updated index action and added new action add_to_index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,21 @@
 class HomeController < ApplicationController
 
   def index
-    render
+    @home_index_list=Homeindex.find(:all)
+  end
+
+  def add_to_index
+    if request.get?
+      render
+    else  
+      index=Homeindex.new(:month => params[:month])
+      if index.valid?
+         index.save
+         flash[:notice] = "Home Index updated successfully"
+      else
+         flash[:error] = index.errors.full_messages.join(', ')
+      end
+    end    
   end
 
 end
