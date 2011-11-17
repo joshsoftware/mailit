f1c1af6f0a8b38df8a9e5e41ec17011e48984682
Ninad Pol <ninad@joshsoftware.com>
2011-11-17 12:30:35 +0530

    removed action for add to index as it is no longer needed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,18 +4,4 @@ class HomeController < ApplicationController
     @home_index_list=Homeindex.find(:all)
   end
 
-  def add_to_index
-    if request.get?
-      render
-    else  
-      index=Homeindex.new(:month => params[:month])
-      if index.valid?
-         index.save
-         flash[:notice] = "Home Index updated successfully"
-      else
-         flash[:error] = index.errors.full_messages.join(', ')
-      end
-    end    
-  end
-
 end
