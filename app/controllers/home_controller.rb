6089773a8f3224df78777bcc0d68a6b2ee6b9f1e
Ninad Pol <ninad@joshsoftware.com>
2011-11-17 18:55:02 +0530

    added pagination to the home index list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @home_index_list=Homeindex.find(:all)
+    @home_index_list=Homeindex.paginate(:page => params[ :page ],:per_page => 5).order('created_at DESC')
   end
 
 end
