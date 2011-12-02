3b8246d114245de5052f9061bbfdefe996d599ed
Ninad Pol <ninad@joshsoftware.com>
2011-12-02 17:29:26 +0530

    added manage action

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -4,6 +4,9 @@ class NewslettersController < ApplicationController
     @news_index_list=Newsletter.where(:type_of_mailer =>"database").paginate(:page => params[ :page ],:per_page => 10).order('created_at DESC')
   end
 
+  def manage
+  end
+
   def send_newsletters
 
     if request.get?
