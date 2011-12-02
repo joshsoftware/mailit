0af2b970377ffe8b34c4220914c2b466c8fe7658
Ninad Pol <ninad@joshsoftware.com>
2011-12-02 19:49:22 +0530

    added basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  USER, PASSWORD = 'newsletter', 'josh123'
+
+  private
+  def authentication_check
+    authenticate_or_request_with_http_basic do |user, password|
+      user == USER && password == PASSWORD
+    end
+  end
+
 end
