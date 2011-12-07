7fc0e6aa3ce2c0cb726637240f3a71951a401c28
Ninad Pol <ninad@joshsoftware.com>
2011-12-07 18:22:04 +0530

    updated method for equality checking

--- spec/controllers/newsletters_controller_spec.rb
+++ spec/controllers/newsletters_controller_spec.rb
@@ -16,32 +16,32 @@ describe NewslettersController do
   context "should validate for" do
     it "subject and template when no values are provided" do
       post :send_newsletters
-      flash[:error].should =="Mailer subject can't be blank, Template file name must be set"
+      flash[:error].should eq("Mailer subject can't be blank, Template file name must be set")
       ActionMailer::Base.deliveries.should be_empty
     end
 
     it "subject when not provided" do
       post :send_newsletters,:subject =>'',:template =>fixture_file_upload("spec/test.html", mime_type = nil, binary = false),:send_mail =>'test'
-      flash[:error].should =="Mailer subject can't be blank"
+      flash[:error].should eq("Mailer subject can't be blank")
       ActionMailer::Base.deliveries.should be_empty
     end
 
     it "template when not provided" do
       post :send_newsletters,:subject =>'TestSubject123',:template =>'',:send_mail =>'test'
-      flash[:error].should =="Template file name must be set"
+      flash[:error].should eq("Template file name must be set")
       ActionMailer::Base.deliveries.should be_empty
     end 
 
     it "email adress when not provided" do
       post :send_newsletters,:subject =>'TestSubject123',:template =>fixture_file_upload("spec/test.html", mime_type = nil, binary = false),:send_mail =>'test'
-      flash[:error].should =="Please enter all mandatory fields(*)"
+      flash[:error].should eq("Please enter all mandatory fields(*)")
       ActionMailer::Base.deliveries.should be_empty
     end
 
     it "csv when not provided" do
       post :send_newsletters,:subject =>'TestSubject123',:template =>fixture_file_upload("spec/test.html", mime_type = nil, binary = false),
         :send_mail =>'externaldb'     
-      flash[:error].should =="Please enter all mandatory fields(*)"
+      flash[:error].should eq("Please enter all mandatory fields(*)")
       ActionMailer::Base.deliveries.should be_empty
     end 
 
@@ -51,32 +51,32 @@ describe NewslettersController do
     it "test mail to single user" do
       post :send_newsletters,:subject =>'TestSubject123',:template =>fixture_file_upload("spec/test.html", mime_type = nil, binary = false),
         :send_mail =>'test',:test_email_address=>'joshsoftwaretest1@gmail.com'
-      flash[:notice].should==$email_success_notification
+      flash[:notice].should eq($email_success_notification)
       ActionMailer::Base.deliveries.should_not be_empty
-      ActionMailer::Base.deliveries.count.should == 1
+      ActionMailer::Base.deliveries.count.should eq(1)
     end
 
     it "test mail to multiple users" do
       post :send_newsletters,:subject =>'TestSubject123',:template =>fixture_file_upload("spec/test.html", mime_type = nil, binary = false),
         :send_mail =>'test',:test_email_address=>'joshsoftwaretest1@gmail.com,joshsoftwaretest2@gmail.com'
-      flash[:notice].should==$email_success_notification
+      flash[:notice].should eq($email_success_notification)
       ActionMailer::Base.deliveries.should_not be_empty
-      ActionMailer::Base.deliveries.count.should == 2
+      ActionMailer::Base.deliveries.count.should eq(2)
     end    
 
     it "mail to external database" do
       post :send_newsletters,:subject =>'TestSubject123',:template =>fixture_file_upload("spec/test.html", mime_type = nil, binary = false),
         :send_mail =>'externaldb',:csv_upload =>fixture_file_upload("spec/test.csv", mime_type = nil, binary = false)
-      flash[:notice].should==$email_success_notification
+      flash[:notice].should eq($email_success_notification)
       #Below the delivery count is checked as 1 because the input file "test.csv" provided above contains only one email
-      ActionMailer::Base.deliveries.count.should == 1
+      ActionMailer::Base.deliveries.count.should eq(1)
     end
 
 =begin    
     #We cannot validate this scenario using "ActionMailer::Base.deliveries" as this triggers rake task in the background
     it "mail to database" do
       post :send_newsletters,:subject =>'TestSubject123',:template =>fixture_file_upload("spec/test.html", mime_type = nil, binary = false),:send_mail =>'database'
-      flash[:notice].should==$db_email_success_notification
+      flash[:notice].should eq($db_email_success_notification)
     end
 =end
 
