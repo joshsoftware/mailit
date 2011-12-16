5a05afcaf461063055a6abc394edba46fe6cdcff
Ninad Pol <ninad@joshsoftware.com>
2011-12-16 17:58:05 +0530

    added customized validation message

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,6 +1,6 @@
 class Newsletter < ActiveRecord::Base
   validates_presence_of :mailer_subject
-  validates_attachment_presence :template
+  validates_attachment_presence :template ,:message => "must be uploaded"
   
   # Paperclip
   has_attached_file :template
