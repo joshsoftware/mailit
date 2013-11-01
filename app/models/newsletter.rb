d609869d74ba85ec4c2a91e9205efb2551fe2816
Ninad Pol <ninad@joshsoftware.com>
2013-11-01 16:56:52 +0530

    added code to copy the uploaded template to /public directory

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -4,4 +4,9 @@ class Newsletter < ActiveRecord::Base
   
   # Paperclip
   has_attached_file :template
+
+  # Move the uploaded template to /public directory
+  after_save(:on => :create) do
+    system "cp", self.template.path.to_s, "#{Rails.root.join('public').to_s}"
+  end
 end
