060f950d361dc64c038b962011c8a5b0c023504c
Ninad Pol <ninad@joshsoftware.com>
2013-11-21 15:31:29 +0530

    optimized code to reduce complexity

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -11,53 +11,44 @@ class SubscribersController < ApplicationController
   end
 
   def add_subscriber
-    if request.get?
-      render
-    else
-      @subscriber = Subscriber.new(:first_name => params[:first_name],:last_name => params[:last_name],:email => params[:email])
-      if @subscriber.valid?
-        @subscriber.save
-        flash[:notice] = I18n.t('notice.subscriber_added_success')
-        @subscriber = Subscriber.new
-      else
-        flash[:error] = @subscriber.errors.full_messages.join(' , ')
-      end
-      render
-    end   
+    parameter = params
+
+    render and return if request.get?
+
+    @subscriber = Subscriber.new(:first_name => parameter[:first_name],:last_name => parameter[:last_name],:email => parameter[:email])
+    flash[:error] = @subscriber.errors.full_messages.join(' , ') and return unless @subscriber.valid?
+
+    @subscriber.save
+    flash[:notice] = I18n.t('notice.subscriber_added_success')
+    @subscriber = Subscriber.new
+    render
   end
 
   def import_subscribers
-    if request.get?
-      render
-    else 
-      if params[:subscriber_csv].blank?
-        flash[:error] = I18n.t('error.csv_upload')
-      else
-        begin
-          Subscriber.import(params[:subscriber_csv])
-          flash[:notice] = "Import Successful. [#{Subscriber.import_count}] users imported"
-        rescue Exception => e
-          flash[:error] = I18n.t('error.csv_upload_only')
-        end
-      end
-    end  
+    parameter = params
+
+    render and return if request.get?
+    flash[:error] = I18n.t('error.csv_upload') and return if parameter[:subscriber_csv].blank?
+
+    begin
+      Subscriber.import(parameter[:subscriber_csv])
+      flash[:notice] = "Import Successful. [#{Subscriber.import_count}] users imported"
+    rescue Exception => e
+      flash[:error] = I18n.t('error.csv_upload_only')
+    end
   end
 
   def remove_bounced_subscribers
-    if request.get?
-      render
-    else 
-      if params[:bounces_csv].blank?
-        flash[:error] = I18n.t('error.csv_upload')
-      else
-        begin
-          Subscriber.remove_bounced_users(params[:bounces_csv])
-          flash[:notice] = "[#{Subscriber.removal_count}] users removed"
-        rescue Exception => e
-          flash[:error] = I18n.t('error.csv_upload_only')
-        end
-      end
-    end  
+    parameter = params
+
+    render and return if request.get?
+    flash[:error] = I18n.t('error.csv_upload') and return if parameter[:bounces_csv].blank?
+    begin
+      Subscriber.remove_bounced_users(parameter[:bounces_csv])
+      flash[:notice] = "[#{Subscriber.removal_count}] users removed"
+    rescue Exception => e
+      flash[:error] = I18n.t('error.csv_upload_only')
+    end
   end
 
   #below method is use to take backup of current database on heroku
